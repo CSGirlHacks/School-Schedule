1b8c94b348c1483be8ba61d9b27cab08db68213d
Michaela Radel <mlcornai@buffalo.edu>
2020-10-25 10:05:12 -0400

    Final commit for Devpost

--- src/FileAndFileReader/Reader.scala
+++ src/FileAndFileReader/Reader.scala
@@ -1,8 +1,5 @@
 package FileAndFileReader
 
-import FileAndFileReader.Reader.splitIfNeeded
-import scala.util.Random
-
 object Reader {
 
   def findSeventhGrade(filename:String): List[String] = {
@@ -11,18 +8,17 @@ object Reader {
     for (line <- bufferedSource.getLines) {
       val cols = line.split(",").map(_.trim)
       // do whatever you want with the columns here
-      val name = (s"${cols(0)} ${cols(1)}")
+      val name = s"${cols(0)} ${cols(1)}"
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
       val matches = Map(name -> classes)
       for (entry <- matches.values){
         if (entry(1) == "MTH7") {
           seventhGrade = seventhGrade + name + ","
-          }
         }
       }
+    }
     seventhGrade.split(",").toList
   }
-
   def splitSeventhGradeBand(filename:String): List[List[String]] = {
     val seventhGrade = findSeventhGrade(filename)
     val bufferedSource = io.Source.fromFile(filename)
@@ -32,7 +28,7 @@ object Reader {
       // do whatever you want with the columns here
       val namesMasterList = List(cols(0) + " "  + cols(1))
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
-      val matches = Map(namesMasterList -> classes)
+      //val matches = Map(namesMasterList -> classes)
       for (allNames <- namesMasterList){
         for (name <- seventhGrade) {
           if (name == allNames) {
@@ -46,7 +42,6 @@ object Reader {
     val seventhBand = band.split(",").toList
     splitIfNeeded(18, seventhBand)
   }
-
   def splitSeventhGradeMusic(filename:String): List[List[String]] = {
     val seventhGrade = findSeventhGrade(filename)
     val bufferedSource = io.Source.fromFile(filename)
@@ -56,7 +51,7 @@ object Reader {
       // do whatever you want with the columns here
       val namesMasterList = List(cols(0) + " "  + cols(1))
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
-      val matches = Map(namesMasterList -> classes)
+      //val matches = Map(namesMasterList -> classes)
       for (allNames <- namesMasterList){
         for (name <- seventhGrade) {
           if (name == allNames) {
@@ -70,14 +65,13 @@ object Reader {
     val seventhMusic = music.split(",").toList
     splitIfNeeded(18, seventhMusic)
   }
-
   def findEighthGrade(filename:String): List[String] = {
     val bufferedSource = io.Source.fromFile(filename)
     var eighthGrade: String = ""
     for (line <- bufferedSource.getLines) {
       val cols = line.split(",").map(_.trim)
       // do whatever you want with the columns here
-      val name = (s"${cols(0)} ${cols(1)}")
+      val name = s"${cols(0)} ${cols(1)}"
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
       val matches = Map(name -> classes)
       for (entry <- matches.values){
@@ -88,7 +82,6 @@ object Reader {
     }
     eighthGrade.split(",").toList
   }
-
   def splitEighthGradeBand(filename:String): List[List[String]] = {
     val eighthGrade = findEighthGrade(filename)
     val bufferedSource = io.Source.fromFile(filename)
@@ -98,21 +91,20 @@ object Reader {
       // do whatever you want with the columns here
       val namesMasterList = List(cols(0) + " "  + cols(1))
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
-      val matches = Map(namesMasterList -> classes)
+      //val matches = Map(namesMasterList -> classes)
       for (allNames <- namesMasterList){
         for (name <- eighthGrade) {
           if (name == allNames) {
             if (classes(3) == "BND") {
               band = band + name + ","
-              }
             }
           }
         }
       }
+    }
     val eighthBand = band.split(",").toList
     splitIfNeeded(18, eighthBand)
   }
-
   def splitEighthGradeMusic(filename:String): List[List[String]] = {
     val eighthGrade = findEighthGrade(filename)
     val bufferedSource = io.Source.fromFile(filename)
@@ -122,7 +114,7 @@ object Reader {
       // do whatever you want with the columns here
       val namesMasterList = List(cols(0) + " "  + cols(1))
       val classes = List(cols(2), cols(3), cols(4), cols(5), cols(6))
-      val matches = Map(namesMasterList -> classes)
+      //val matches = Map(namesMasterList -> classes)
       for (allNames <- namesMasterList){
         for (name <- eighthGrade) {
           if (name == allNames) {
@@ -136,7 +128,6 @@ object Reader {
     val eighthMusic = music.split(",").toList
     splitIfNeeded(18, eighthMusic)
   }
-
   def splitIfNeeded(maxSize:Int, classList: List[String]) : List[List[String]] = {
     if (classList.length > maxSize) {
       val splitIndex = classList.length/2
@@ -150,7 +141,7 @@ object Reader {
   }
 
   def generateCohorts(filename: String): List[Cohort] = {
-    val filename = "src/FileAndFileReader/Dummy Schedule.csv"
+    //val filename = "src/FileAndFileReader/Dummy Schedule.csv"
     val seventhGradeBand = splitSeventhGradeBand(filename)
     val seventhGradeBandA = new Cohort (seventhGradeBand(0))
     val seventhGradeBandB = new Cohort (seventhGradeBand(1))
@@ -167,17 +158,56 @@ object Reader {
     listOfCohorts
   }
 
-def remixSchedule (input: Iterable[List[String]]) : Unit = {
-    val indexList = Array(0,1, 2, 3, 4)
-  val rand = new Random(System.currentTimeMillis())
-  val random_index = rand.nextInt(indexList.length)
-  val result = indexList(random_index)
-  println(result)
+    def createClassroom(filename:String):Map[String,Map[List[String],List[String]]]={
+
+      var schedule:List[Map[List[String],List[String]]]=List()
+      val arrayOfCohorts = generateCohorts(filename)
+      for (cohort <- arrayOfCohorts) {
+        schedule=schedule:+cohort.getSchedule(filename)
+      }
+      val indexList = Array(1,2, 3, 4, 5, 6, 7, 8)
+      var li:List[String]=List()
+      var mappedClass:Map[String, Map[List[String],List[String]]]=Map()
+      for(i<-indexList){
+        li=li:+("Class "+i)
+      }
+      mappedClass=li.zip(schedule).toMap
+      mappedClass
+    }
+
+//  def count(students:List[String]):Int= {
+//    println(students)
+//
+//  }
+
+  def GymAssignment(c:Map[String, Map[List[String],List[String]]]):Unit={
+    for((k,v)<-c){
+      println("\n" + k+ " has gym")
+      println("Students in "+ k + " are:" + v.keys)
+//      var x=0
+//      for(i<-v.keys){
+//        println(i)
+//        count(i)
+//      }
+//      println("Number of kids outside:" + x)
+//      if(x<20){
+//        println("Doesn't exceed 20. Social Distance can be maintained")
+//      }
+//      else{
+//        println("Warning : More than 20 students. Social Distancing cannot be maintained")
+//      }
+      Thread.sleep(2000)
+    }
+    println("Day is over")
   }
 
   def main(args: Array[String]): Unit = {
+    println("Classes and their Timetable (subjected to change due to the pandemic)\n")
     val arrayOfCohorts = generateCohorts("src/FileAndFileReader/Dummy Schedule.csv")
-    for (cohort <- arrayOfCohorts)
+    for (cohort <- arrayOfCohorts) {
       cohort.getSchedule("src/FileAndFileReader/Dummy Schedule.csv")
+    }
+      val classroom=createClassroom("src/FileAndFileReader/Dummy Schedule.csv")
+      GymAssignment(classroom)
   }
 }
